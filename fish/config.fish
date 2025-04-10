7afe9389c07e8d165346ca1db44f28a33a6f0b49
Sven Paulsen <info@sven-paulsen.de>
2025-04-10 15:43:45 +0200

    add jdk config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 # NVM setup
 
 function nvm
-	bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+        bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
 end
 
 set -x NVM_DIR ~/.nvm
 nvm use default --silent
+
+# JDK setup
+
+set -x JAVA_HOME (brew --prefix openjdk)/libexec/openjdk.jdk/Contents/Home
+set -x PATH $JAVA_HOME/bin $PATH
